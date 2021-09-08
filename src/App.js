56656f55271afec1bf2aa47088bb41bead81f765
Hawar-se <55180716+Hawar-se@users.noreply.github.com>
2021-09-08 13:39:09 +0200

    Add/fix some style.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
 import JSON from "./users.json";
 import Typography from "@material-ui/core/Typography";
-import {
-  Grid,
-  Card,
-  CardHeader,
-  CardActionArea,
-} from "@material-ui/core/";
+import { Grid, Card, CardHeader, CardActionArea } from "@material-ui/core/";
 import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: "#4386eb",
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
+
   return (
     <div>
-      <header className="App_header">
+      <header className={classes.App_header}>
         <Typography variant="h3">Users</Typography>
       </header>
       {/* Grid has been used to adapt to the screen size */}
@@ -26,16 +32,24 @@ function App() {
       >
         {/* It is sized relative to its parent element based on a 12-column grid layout, I used 3 breakpoints */}
 
-{/* Map through element from JSON file and display it on card  */}
+        {/* Map through element from JSON file and display it on card  */}
         {JSON.map((elem) => (
           <Grid item xs={12} sm={6} md={6} key={JSON.indexOf(elem)}>
-            <Card>
+            <Card className={classes.root}>
               <CardActionArea>
                 <CardHeader
-                  titleTypographyProps={{ variant: "h5" }}
                   title={` ${elem.name}`}
-                  subheader={` ${elem.company.name}`}
-                  avatar={<Avatar aria-label="recipe"></Avatar>}
+                  titleTypographyProps={{ variant: "h4", color: "black" }}
+
+                  subheader={`( ${elem.company.name})`}
+                  subheaderTypographyProps={{ variant: "h6" }}
+                  // MuiCardHeader-subheader={{display:inline}}
+                  avatar={
+                    <Avatar
+                      aria-label="recipe"
+                      className={classes.avatar}
+                    ></Avatar>
+                  }
                 />
               </CardActionArea>
             </Card>
