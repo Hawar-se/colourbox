23f077945eac8bd5082d57518e3424a499a0d16a
Hawar-se <55180716+Hawar-se@users.noreply.github.com>
2021-09-08 12:17:41 +0200

    Add files via upload
    Layout of user based on the grid.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import Typography from "@material-ui/core/Typography";
+import { Grid, Card } from "@material-ui/core/";
 
 function App() {
   return (
-    <div className="App">
-    
-       
+    <div>
+      <header className="App_header">
+        <Typography variant="h3">Users</Typography>
+      </header>
+      {/* Grid has been used to adapt to the screen size */}
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+        {/* It is sized relative to its parent element based on a 12-column grid layout, I used 3 breakpoints */}
+        <Grid item sm={12} md={6} lg={3}>
+          <Card></Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
