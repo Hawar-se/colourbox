b7d2c914ea8aab6e1651340dd52a956088cfd521
Hawar-se <55180716+Hawar-se@users.noreply.github.com>
2021-09-08 12:37:55 +0200

    map() has been used to create an array on each element present in the parent array.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
+import JSON from "./users.json";
 import Typography from "@material-ui/core/Typography";
-import { Grid, Card } from "@material-ui/core/";
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardActionArea,
+} from "@material-ui/core/";
+import Avatar from "@material-ui/core/Avatar";
 
 function App() {
   return (
@@ -9,6 +16,7 @@ function App() {
         <Typography variant="h3">Users</Typography>
       </header>
       {/* Grid has been used to adapt to the screen size */}
+
       <Grid
         container
         spacing={2}
@@ -17,9 +25,22 @@ function App() {
         alignItems="flex-start"
       >
         {/* It is sized relative to its parent element based on a 12-column grid layout, I used 3 breakpoints */}
-        <Grid item sm={12} md={6} lg={3}>
-          <Card></Card>
-        </Grid>
+
+{/* Map through element from JSON file and display it on card  */}
+        {JSON.map((elem) => (
+          <Grid item xs={12} sm={6} md={6} key={JSON.indexOf(elem)}>
+            <Card>
+              <CardActionArea>
+                <CardHeader
+                  titleTypographyProps={{ variant: "h5" }}
+                  title={` ${elem.name}`}
+                  subheader={` ${elem.company.name}`}
+                  avatar={<Avatar aria-label="recipe"></Avatar>}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
